9a532a28ae7e054ff8bcef58e0695dbdd3969b24
Bodzio0019 <nikolas.ed@onet.eu>
2024-02-04 02:52:55 +0100

    Dodano folder DATA i plik DATA.JS i stworzono tam bazę danych, dodano pliki .JPG, napisano cały JS i popraiono HTML i CSS

---
+++ data/data.js
@@ -0,0 +1,12 @@
+export const games = [
+    {
+        img: "Bloodborne",
+        name: "Bloodborne",
+        content: "Hunt your nightmares as you search for answers in the ancient city of Yharnam, now cursed with a strange endemic illness spreading through the streets like wildfire. Danger, death and madness lurk around every corner of this dark and horrific world, and you must discover its darkest secrets in order to survive."
+    },
+    {
+        img: "Witcher",
+        name: "Witcher 3",
+        content: "You are Geralt, from the city of Rivia. All around you the towns and settlements of the Northern Kingdoms are being razed to the ground, as an otherworldly invading army known only as the Wild Hunt leaves a trail of blood-soaked destruction in its wake."
+    }
+]
--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello");
+import { games } from "./data/data.js";
+
+const ikona_wr = document.body.querySelector(".ikona");
+const tekst_wr = document.body.querySelector(".tekst");
+const left = document.body.querySelector(".left");
+const right = document.body.querySelector(".right");
+let count = 0;
+
+function render() {
+    ikona_wr.innerHTML = `<img src="img/${games[count].img}.jpg" alt="Okładka gry ${games[count].name}" />
+    <h2>${games[count].name}</h2>`;
+    tekst_wr.innerHTML = `<p>${games[count].content}</p>`;
+};
+render();
+
+right.addEventListener("click", function() {
+    if (count < games.length - 1) {
+        count++;
+        render();
+    } else {
+        count = 0;
+        render();
+    }
+});
+left.addEventListener("click", function() {
+    if (count > 0 ) {
+        count--;
+        render();
+    } else {
+        count = games.length - 1;
+        render();
+    }
+});
